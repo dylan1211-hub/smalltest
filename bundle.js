ab7b8751c6a9ea32c59524e070efd92cfcd8a10e
dylan1211-hub <hsteven2538@gmail.com>
2021-12-04 15:27:34 +0800

    1204test4:

--- bundle.js
+++ bundle.js
@@ -8604,7 +8604,7 @@ window.onload = () => {
 
     var https = require("https");
 
-    var url = "https://62a1-2001-b011-e004-1ca5-f434-5588-1508-28a3.ngrok.io/smalltest";
+    var url = "https://97f1-2001-b011-e004-1ca5-f434-5588-1508-28a3.ngrok.io/smalltest";
 
     var data = "";
    https.get(url, function (response) 
