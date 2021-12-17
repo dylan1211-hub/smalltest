bef67242007f5b34b374d9e41767b1d3ec86875e
dylan1211-hub <hsteven2538@gmail.com>
2021-12-17 10:15:25 +0800

    1217test10

--- bundle.js
+++ bundle.js
@@ -8604,7 +8604,7 @@ window.onload = () => {
 
     var https = require("https");
 
-    var url = "https://2d49-2001-b011-e004-19c8-a929-873b-aa31-2eab.ngrok.io/smalltest";
+    var url = "https://e0fd-2001-b011-e004-1db4-64c0-3cfb-9221-9056.ngrok.io/position";
 
     var data = "";
    https.get(url, function (response) 
@@ -8631,7 +8631,7 @@ window.onload = () => {
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
-    for(let i = 0 ; i<=7;i++){
+    for(let i = 0 ; i<=10;i++){
         const latitude = places[i].lat;  //修改後
         const longitude = places[i].lon; //修改後
 
@@ -8650,32 +8650,32 @@ function renderPlaces(places) {
         //const touchListener = ;
 
         icon.addEventListener('touchend', function () {
-            //ev.stopPropagation();
-            //ev.preventDefault();
+            ev.stopPropagation();
+            ev.preventDefault();
 
-            //const name = ev.target.getAttribute('name');
+            const name = ev.target.getAttribute('name');
 
-            //const el = ev.detail.intersection && ev.detail.intersection.object.el;
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-            //if (el && el === ev.target) {
+            if (el && el === ev.target) {
                 window.alert("有喔，有點到喔!");
 
-                //const label = document.createElement('span');
-                //const container = document.createElement('div');
-                //container.setAttribute('id', 'place-label');
-                //label.innerText = name;
-                //container.appendChild(label);
-                //document.body.appendChild(container);
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
 
-                //setTimeout(() => {
-                    //container.parentElement.removeChild(container);
-                //}, 1500);
-            //}
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
         });
     
         scene.appendChild(icon);
     }
-/*
+
     for(let i = 11 ; i<=20;i++){
         const latitude = places[i].lat;  //修改後
         const longitude = places[i].lon; //修改後
@@ -8689,7 +8689,7 @@ function renderPlaces(places) {
         icon.setAttribute('src','https://dylan1211-hub.github.io/smalltest/assets/r2.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale','5, 5');
+        icon.setAttribute('scale','20, 20');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
@@ -8735,7 +8735,7 @@ function renderPlaces(places) {
         icon.setAttribute('src','https://dylan1211-hub.github.io/smalltest/assets/r3.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale','5, 5');
+        icon.setAttribute('scale','20, 20');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
@@ -8781,7 +8781,7 @@ function renderPlaces(places) {
         icon.setAttribute('src','https://dylan1211-hub.github.io/smalltest/assets/r4.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale','5, 5');
+        icon.setAttribute('scale','20, 20');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
