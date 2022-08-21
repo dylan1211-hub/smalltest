0617928d311ffd061aa627b7dc1baa4aee7f9f4e
cdxvy30 <cdxvy30@gmail.com>
2022-08-21 22:14:05 +0800

    Test for Conv Data

--- bus_test/createTable.sql
+++ bus_test/createTable.sql
@@ -1,3 +1,4 @@
+-- Restaurants (static data)
 CREATE TABLE Restaurant(
   fid SERIAL PRIMARY KEY,
   id CHAR(5),
@@ -10,9 +11,10 @@ CREATE TABLE Restaurant(
   category CHAR(130),
   consume CHAR(5)
 );
+-- lat, lon should be double precision, but some of them are missing value.
+-- Try to insert NULL.
 
--- lat, lon should be double precision
-
+-- Convenience Stores (static data)
 CREATE TABLE ConvenienceStore (
   fid SERIAL PRIMARY KEY,
   address_name CHAR(100),
@@ -20,3 +22,29 @@ CREATE TABLE ConvenienceStore (
   lon FLOAT,
   category CHAR(10)
 );
+
+-- Routes (static data)
+CREATE TABLE allRoute(
+  fid SERIAL PRIMARY KEY,
+  RouteUID CHAR(100),
+  RouteID CHAR(100),
+  RouteName_zh CHAR(100),
+  RouteName_en CHAR(100),
+  HasSubRoutes BOOLEAN,
+  RouteType INT,
+  TicketPriceDescription_zh CHAR(100),
+  TicketPriceDescription_en CHAR(100),
+  DepartureStopName_zh CHAR(100),
+  DepartureStopName_en CHAR(100),
+  StartStop_zh CHAR(100),
+  StartStop_en CHAR(100),
+  EndStop_zh CHAR(100),
+  EndStop_en CHAR(100),
+  RouteURL CHAR(100),
+  LiveBusURL CHAR(100),
+  RouteImageURL CHAR(100),
+  FareURL CHAR(100),
+  RouteDistance INT
+);
+
+-- 
